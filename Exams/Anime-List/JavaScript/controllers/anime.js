e56a5f9c5cfa21c90e449d6ee5d9f8ee64c46629
AtanasovAtanas <atanas4537@gmail.com>
2017-11-19 20:59:47 +0200

    Add exams in JavaScript

---
+++ Exams/Anime-List/JavaScript/controllers/anime.js
@@ -0,0 +1,40 @@
+const Anime = require('../models/Anime');
+
+module.exports = {
+    index: (req, res) => {
+        Anime.find().then(anime => {
+            res.render('anime/index', {'animes': anime});
+        })
+    },
+    createGet: (req, res) => {
+        res.render('anime/create');
+    },
+    createPost: (req, res) => {
+        let anime = req.body;
+
+        if (!anime.rating
+            || !anime.name
+            || anime.description === "...") {
+            res.redirect('/create');
+            return;
+        }
+
+        Anime.create(anime).then(anime => {
+            res.redirect('/');
+        })
+    },
+    deleteGet: (req, res) => {
+        let id = req.params.id;
+
+        Anime.findById(id).then(anime => {
+            res.render('anime/delete', anime)
+        });
+    },
+    deletePost: (req, res) => {
+        let id = req.params.id;
+
+        Anime.findByIdAndRemove(id).then(anime => {
+            res.redirect('/');
+        });
+    }
+};
---
+++ Exams/IMDB/JavaScriptSkeleton/controllers/film.js
@@ -0,0 +1,64 @@
+const Film = require('../models/Film');
+
+module.exports = {
+    index: (req, res) => {
+        Film.find().then(films => {
+            res.render('film/index', {'films': films})
+        });
+    },
+    createGet: (req, res) => {
+        res.render('film/create');
+    },
+    createPost: (req, res) => {
+        let filmArgs = req.body;
+
+        if (!filmArgs.name
+            || !filmArgs.genre
+            || !filmArgs.director
+            || !filmArgs.year) {
+            res.render('film/create');
+            return;
+        }
+
+        Film.create(filmArgs).then(film => {
+            res.redirect('/');
+        })
+    },
+    editGet: (req, res) => {
+        let id = req.params.id;
+
+        Film.findById(id).then(film => {
+            res.render('film/edit', film)
+        });
+    },
+    editPost: (req, res) => {
+        let id = req.params.id;
+        let film = req.body;
+
+        if (!film.name
+            || !film.genre
+            || !film.director
+            || !film.year) {
+            res.redirect('/');
+            return;
+        }
+
+        Film.findByIdAndUpdate(id, film).then(film => {
+            res.redirect('/');
+        })
+    },
+    deleteGet: (req, res) => {
+        let id = req.params.id;
+
+        Film.findById(id).then(film => {
+            res.render('film/delete', film)
+        });
+    },
+    deletePost: (req, res) => {
+        let id = req.params.id;
+
+        Film.findByIdAndRemove(id).then(film => {
+            res.redirect('/')
+        });
+    }
+};
