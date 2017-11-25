74c848901cc45f400099605413f5dee07bfcec7f
AtanasovAtanas <atanas4537@gmail.com>
2017-11-25 12:23:05 +0200

    Update calculator

---
+++ 09-PHP-Symfony/Calculator/var/cache/dev/twig/74/741dc08b9d3bbd70b46b761f1f43968fc79addd850b214b534618564b9fca8aa.php
@@ -0,0 +1,134 @@
+<?php
+
+/* @WebProfiler/Collector/memory.html.twig */
+class __TwigTemplate_f74e3698d5966af8ba7472bd540386aa7e11ff95f87420763c72e8d207a6ab03 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("@WebProfiler/Profiler/layout.html.twig", "@WebProfiler/Collector/memory.html.twig", 1);
+        $this->blocks = array(
+            'toolbar' => array($this, 'block_toolbar'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "@WebProfiler/Profiler/layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_227d0c9b97b385199e551e443fbd454aab585a3f81464fb9e3f03047e3fe36a9 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_227d0c9b97b385199e551e443fbd454aab585a3f81464fb9e3f03047e3fe36a9->enter($__internal_227d0c9b97b385199e551e443fbd454aab585a3f81464fb9e3f03047e3fe36a9_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Collector/memory.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_227d0c9b97b385199e551e443fbd454aab585a3f81464fb9e3f03047e3fe36a9->leave($__internal_227d0c9b97b385199e551e443fbd454aab585a3f81464fb9e3f03047e3fe36a9_prof);
+
+    }
+
+    // line 3
+    public function block_toolbar($context, array $blocks = array())
+    {
+        $__internal_07f2ab0b0776875c26db1b214f4fa8711b72d22b759cdb2421b77d535308910d = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_07f2ab0b0776875c26db1b214f4fa8711b72d22b759cdb2421b77d535308910d->enter($__internal_07f2ab0b0776875c26db1b214f4fa8711b72d22b759cdb2421b77d535308910d_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "toolbar"));
+
+        // line 4
+        echo "    ";
+        ob_start();
+        // line 5
+        echo "        ";
+        $context["status_color"] = ((((($this->getAttribute((isset($context["collector"]) ? $context["collector"] : $this->getContext($context, "collector")), "memory", array()) / 1024) / 1024) > 50)) ? ("yellow") : (""));
+        // line 6
+        echo "        ";
+        echo twig_include($this->env, $context, "@WebProfiler/Icon/memory.svg");
+        echo "
+        <span class=\"sf-toolbar-value\">";
+        // line 7
+        echo twig_escape_filter($this->env, sprintf("%.1f", (($this->getAttribute((isset($context["collector"]) ? $context["collector"] : $this->getContext($context, "collector")), "memory", array()) / 1024) / 1024)), "html", null, true);
+        echo "</span>
+        <span class=\"sf-toolbar-label\">MB</span>
+    ";
+        $context["icon"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+        // line 10
+        echo "
+    ";
+        // line 11
+        ob_start();
+        // line 12
+        echo "        <div class=\"sf-toolbar-info-piece\">
+            <b>Peak memory usage</b>
+            <span>";
+        // line 14
+        echo twig_escape_filter($this->env, sprintf("%.1f", (($this->getAttribute((isset($context["collector"]) ? $context["collector"] : $this->getContext($context, "collector")), "memory", array()) / 1024) / 1024)), "html", null, true);
+        echo " MB</span>
+        </div>
+
+        <div class=\"sf-toolbar-info-piece\">
+            <b>PHP memory limit</b>
+            <span>";
+        // line 19
+        echo twig_escape_filter($this->env, ((($this->getAttribute((isset($context["collector"]) ? $context["collector"] : $this->getContext($context, "collector")), "memoryLimit", array()) ==  -1)) ? ("Unlimited") : (sprintf("%.0f MB", (($this->getAttribute((isset($context["collector"]) ? $context["collector"] : $this->getContext($context, "collector")), "memoryLimit", array()) / 1024) / 1024)))), "html", null, true);
+        echo "</span>
+        </div>
+    ";
+        $context["text"] = ('' === $tmp = ob_get_clean()) ? '' : new Twig_Markup($tmp, $this->env->getCharset());
+        // line 22
+        echo "
+    ";
+        // line 23
+        echo twig_include($this->env, $context, "@WebProfiler/Profiler/toolbar_item.html.twig", array("link" => (isset($context["profiler_url"]) ? $context["profiler_url"] : $this->getContext($context, "profiler_url")), "name" => "time", "status" => (isset($context["status_color"]) ? $context["status_color"] : $this->getContext($context, "status_color"))));
+        echo "
+";
+        
+        $__internal_07f2ab0b0776875c26db1b214f4fa8711b72d22b759cdb2421b77d535308910d->leave($__internal_07f2ab0b0776875c26db1b214f4fa8711b72d22b759cdb2421b77d535308910d_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Collector/memory.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  83 => 23,  80 => 22,  74 => 19,  66 => 14,  62 => 12,  60 => 11,  57 => 10,  51 => 7,  46 => 6,  43 => 5,  40 => 4,  34 => 3,  11 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{% extends '@WebProfiler/Profiler/layout.html.twig' %}
+
+{% block toolbar %}
+    {% set icon %}
+        {% set status_color = (collector.memory / 1024 / 1024) > 50 ? 'yellow' : '' %}
+        {{ include('@WebProfiler/Icon/memory.svg') }}
+        <span class=\"sf-toolbar-value\">{{ '%.1f'|format(collector.memory / 1024 / 1024) }}</span>
+        <span class=\"sf-toolbar-label\">MB</span>
+    {% endset %}
+
+    {% set text %}
+        <div class=\"sf-toolbar-info-piece\">
+            <b>Peak memory usage</b>
+            <span>{{ '%.1f'|format(collector.memory / 1024 / 1024) }} MB</span>
+        </div>
+
+        <div class=\"sf-toolbar-info-piece\">
+            <b>PHP memory limit</b>
+            <span>{{ collector.memoryLimit == -1 ? 'Unlimited' : '%.0f MB'|format(collector.memoryLimit / 1024 / 1024) }}</span>
+        </div>
+    {% endset %}
+
+    {{ include('@WebProfiler/Profiler/toolbar_item.html.twig', { link: profiler_url, name: 'time', status: status_color }) }}
+{% endblock %}
+";
+    }
+}
